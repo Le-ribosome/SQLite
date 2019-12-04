5533b241a2961c4820826b0e43a485bc69f493b8
Alexis <drouinalexis@hotmail.fr>
2019-12-04 08:30:35 +0100

    suite

--- demo/src/main/java/com/example/demo/database/DatabaseDataWorker.java
+++ demo/src/main/java/com/example/demo/database/DatabaseDataWorker.java
@@ -2,6 +2,7 @@ package com.example.demo.database;
 
 import com.example.demo.database.DBOpenHelper.Constants;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 
 public class DatabaseDataWorker {
@@ -9,27 +10,21 @@ public class DatabaseDataWorker {
 	
 	private SQLiteDatabase mDb;
 	public DatabaseDataWorker(SQLiteDatabase db) {mDb = db;};
-	
-	
-//    private static final String DATABASE_CREATE = "create table "
-//    	    + Constants.MY_TABLE + "(" + Constants.KEY_COL_ID
-//    	    + " integer primary key autoincrement, " + Constants.KEY_COL_AGE
-//    	    + " INTEGER, " + Constants.KEY_COL_NAME + " TEXT, "
-//    	    + Constants.KEY_COL_FIRSTNAME + " TEXT, "
-//    	    + Constants.KEY_COL_EYES_COLOR + " TEXT, "
-//    	    + Constants.KEY_COL_HAIR_COLOR + " TEXT) ";
-    
+	 
 	public void insertHumans() {
 		insertHuman("26", "Minou","Alexis", "Vert", "Noir");
 		insertHuman("25", "Minette","Pauline", "Marron", "Noir");
 	}
 	
-    private void insertCourse(String courseId, String title) {
+    private void insertHuman(String humanId, String age, String name, String firstName, String eyeColor, String hairColor) {
         ContentValues values = new ContentValues();
-        values.put(NoteKeeperDatabaseContract.CourseInfoEntry.COLUMN_COURSE_ID, courseId);
-        values.put(NoteKeeperDatabaseContract.CourseInfoEntry.COLUMN_COURSE_TITLE, title);
-
-        long newRowId = mDb.insert(NoteKeeperDatabaseContract.CourseInfoEntry.TABLE_NAME, null, values);
+        values.put(DBOpenHelper.Constants.KEY_COL_ID, humanId);
+        values.put(DBOpenHelper.Constants.KEY_COL_AGE, age);
+        values.put(DBOpenHelper.Constants.KEY_COL_NAME, name);
+        values.put(DBOpenHelper.Constants.KEY_COL_FIRSTNAME, firstName);
+        values.put(DBOpenHelper.Constants.KEY_COL_EYES_COLOR, eyeColor);
+        values.put(DBOpenHelper.Constants.KEY_COL_HAIR_COLOR, hairColor);
+        long newRowId = mDb.insert(DBOpenHelper.Constants.MY_TABLE, null, values);
     }
     
 }
